5fc8a3086e0711f4a962c9fe93c40ee78cc3e020
fatalnyishod <fatalnyishod@gmail.com>
2020-02-23 14:21:40 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// создание переменных
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	plumber = require('gulp-plumber'),
+	plumberNotifier = require('gulp-plumber-notifier'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssmin = require('gulp-cssmin');
+
+// преобразование sass в css
+gulp.task('sass', function() {
+	return gulp
+		.src('app/sass/**/*.sass')
+		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		.pipe(
+			autoprefixer(['last 4 versions', '> 1%', 'ie 8', 'ie 7'], {
+				cascade: false
+			})
+		)
+		.pipe(gulp.dest('app/static/styles'));
+});
+
+// перезагрузка страницы браузера
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: { baseDir: 'app/' },
+		notify: false
+	});
+});
+
+// минификация css
+gulp.task('minify-css', function() {
+	gulp
+		.src('app/static/styles/**/*.css')
+		.pipe(cssmin())
+		.pipe(gulp.dest('app/static/styles/'));
+});
+
+// наблюдение за изменениями
+gulp.task('watch', ['browser-sync'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/static/js/**/*.js');
+});
+
+// почистить кеш
+gulp.task('clear', function() {
+	return cache.clearAll();
+});
+
+// таск по умолчанию
+gulp.task('default', ['watch']);
